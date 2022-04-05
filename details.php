bc24093172d3a0116a887fbe107a1f1b74abec86
SarahOorts <r0657122@student.thomasmore.be>
2022-04-05 15:15:18 +0200

    feat: created details page for showing detail of data by date with new api call

---
+++ details.php
@@ -0,0 +1,25 @@
+<?php
+    include_once(__DIR__ . "/classes/Picture.php");
+
+    $date = $_GET["date"];
+
+    $picture = new Picture();
+    $p = $picture->getPicture($date);
+    // var_dump($p);
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css"> 
+    <title>Document</title>
+</head>
+<body>
+        <h2><?php echo $p["title"]; ?></h2>
+        <h5><?php echo $date; ?></h5>
+        <img src="<?php echo $p["url"];?>" alt="picture of the day">
+        <p><?php echo $p["explanation"]; ?></p>
+</body>
+</html>
